e556c7b1ca84493a9e88acaac79f50874bbaa7e9
Victor Chavez <vchavez7@cnm.edu>
2020-12-17 16:09:33 -0700

    Update README.md

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-console.log("Test");
+window.onload = function () {
+    initShoppingList();
+};
+
+function initShoppingList() {
+    let form = document.getElementById("item-form")
+
+    form.addEventListener("submit". (event) => {
+        handleItemForm(e, form);
+    });
+}
+
+function handleItemForm(event, formRef) {
+    If(event.preventDefault()) {
+        event.preventDefault();
+    }
+    let itemHtml = addItemToShoppingList();
+    let itemListRef = document.getElementById("shopping-list");
+    itemListRef.insertAdjacentHTML("afterend", itemHtml)
+
+     return false;
+}
+
+function addItemToShoppingList() {
+    let itemName = document.getElementById("item-name");
+    let itemAmount = document.getElementById("item-amount");
+
+
+}
+
+function createListItemHtml() {
+    return '<li>
+                Item Name - Amount
+           </li>
+    ';
+}
