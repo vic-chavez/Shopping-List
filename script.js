3965f10dd5bdb79a682f7c3de36d38112ee9da10
Victor Chavez <vchavez7@cnm.edu>
2020-12-17 17:25:21 -0700

    Update README.md

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ window.onload = function () {
 };
 
 function initShoppingList() {
-    let form = document.getElementById("item-form")
+    let form = document.getElementById("item-form");
 
-    form.addEventListener("submit". (event) => {
-        handleItemForm(e, form);
+    form.addEventListener("submit", (event) => {
+        handleItemForm(event, form);
     });
 }
 
 function handleItemForm(event, formRef) {
-    If(event.preventDefault()); {
+    if(event.preventDefault) {
         event.preventDefault();
     }
 
@@ -24,14 +24,18 @@ function addItemToShoppingList() {
     let itemName = document.getElementById("item-name");
     let itemAmount = document.getElementById("item-amount");
 
-    Let itemHtml = createListItemHtml(itemName.value itemAmount.value);
-    console.log("Item HTML: ",itemHtml);
+    let itemHtml = createListItemHtml(itemName.value, itemAmount.value);
+    console.log("Item HTML: ", itemHtml);
     let itemListRef = document.getElementById("shopping-list");
     itemListRef.insertAdjacentHTML("afterend", itemHtml)
 }
 
 function createListItemHtml(itemName, itemAmount) {
-    return '<li>
+
+    return <li>
                 ${itemName} - ${itemAmount}
-           </li>';
+                <button type="button">Delete Item</button>
+            </li>;
+
 }
+
