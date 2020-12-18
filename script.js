56de8853880aeb8849ac4d48bb7cf82c09e31d02
Victor Chavez <vchavez7@cnm.edu>
2020-12-17 17:25:21 -0700

    Update README.md

--- script.js
+++ script.js
@@ -24,18 +24,20 @@ function addItemToShoppingList() {
     let itemName = document.getElementById("item-name");
     let itemAmount = document.getElementById("item-amount");
 
-    let itemHtml = createListItemHtml(itemName.value, itemAmount.value);
+    let itemHtml = createListItemHtml();
     console.log("Item HTML: ", itemHtml);
     let itemListRef = document.getElementById("shopping-list");
-    itemListRef.insertAdjacentHTML("afterend", itemHtml)
+    itemListRef.insertAdjacentHTML("afterend", itemHtml);
 }
 
 function createListItemHtml(itemName, itemAmount) {
+    return '<li>'
+                Item Name - Amount
 
-    return <li>
-                ${itemName} - ${itemAmount}
-                <button type="button">Delete Item</button>
-            </li>;
+               /li>
+    ';
 
 }
+
+
 
