9162dfbc93fae06c086eb0bb5be2710c42ad0ddb
vic-chavez <75764801+vic-chavez@users.noreply.github.com>
2020-12-17 18:39:23 -0700

    Update script.js

--- script.js
+++ script.js
@@ -24,10 +24,14 @@ function addItemToShoppingList() {
     let itemName = document.getElementById("item-name");
     let itemAmount = document.getElementById("item-amount");
 
-    let itemHtml = createListItemHtml();
+    let itemHtml = createListItemHtml(itemName.value, itemAmount.value);
     console.log("Item HTML: ", itemHtml);
     let itemListRef = document.getElementById("shopping-list");
     itemListRef.insertAdjacentHTML("afterend", itemHtml);
 }
 
+function createListItemHtml(itemName, itemAmount) {
+    return  `<li>
+                ${itemName} - ${itemAmount}
+             </li>`;
 
